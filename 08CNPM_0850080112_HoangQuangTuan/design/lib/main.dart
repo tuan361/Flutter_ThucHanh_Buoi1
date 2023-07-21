26448182a125c7d8f7dd0b7543207d33d264cd36
panda1307 <ngophucthinh1311@gmail.com>
2023-07-21 10:40:15 +0700

    upload code

---
+++ 08CNPM_0850080112_HoangQuangTuan/design/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My app', // used by the OS task switcher
+      home: MyHomeScreen(),
+    ),
+  );
+}
+
+class MyHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trang chu'),
+      ),
+      body: SafeArea(
+        child: Container(
+            alignment: Alignment.center,
+            width: 390,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Hoàng Quang Tu?n 08_CNPM',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                buildRowLayout(),
+                SizedBox(
+                  height: 20,
+                ),
+                Image.network(
+                  'https://media.techmaster.vn/api/static/brbgh4451coepbqoch60/Lf-bvYrA',
+                  width: 200,
+                ),
+
+                ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Nut da duoc bam!')));
+                    },
+                    child: Text('Bam vao day')),
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+Widget buildRowLayout() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        color: Colors.blue,
+        height: 100,
+        width: 100,
+      ),
+      Container(
+        color: Colors.green,
+        height: 100,
+        width: 100,
+      ),
+      Container(
+        color: Colors.orange,
+        height: 100,
+        width: 100,
+      )
+    ],
+  );
+}
